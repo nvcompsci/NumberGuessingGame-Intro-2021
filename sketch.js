fca89363ee11c1faf0370b4599233f5b788852a7
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-12-01 10:02:35 -0500

    revert bad commit

--- sketch.js
+++ sketch.js
@@ -1,33 +1,38 @@
-let colors = ["red","blue","red","yellow","yellow","green","blue","green"]
-let cards = []
-let clicks = 0
+let rand
+let guessBox
+let result
+let triesLeft = 3
 
 function setup() {
-  createP("Matching Game")
-  
-  for (let i=0; i<8; i++) {
-    cards[i] = createButton("Card")
-    //cards[i] means one button
-    //cards[i].style("background-color",colors[i])
-    cards[i].id(i)
-    cards[i].mouseClicked(onCardClick)
-  }  
+  noCanvas()
+  rand = round(random(10))
+  createP("Number Guessing Game")
+  guessBox = createInput()
+  let button = createButton("Go!")
+  button.mouseClicked(checkAnswer)
+  result = createP()
 }
 
-function onCardClick() {
-  clicks++
-  console.log(this.id())
-  
-  this.style("background-color",colors[this.id()])
-  
-  if (clicks > 2) {
-    resetButtons()
-  }
-}
+function checkAnswer() {
+  //alert(guessBox.value())
 
-function resetButtons() {
-  clicks = 0
-  for (let i=0; i < 8; i++) {
-    cards[i].style("background-color","grey")
+  console.log(rand)
+
+  
+    if (guessBox.value() == rand) {
+      result.html("You're right!")
+    } else if (guessBox.value() < rand) {
+      result.html("Too low!")
+      triesLeft--
+    } else {
+      //tell user their guess is too high
+      result.html("Too high!")
+      triesLeft--
+    }
+
+  if (triesLeft <= 0) {
+    result.html("You lose!")
   }
+  
+  
 }
